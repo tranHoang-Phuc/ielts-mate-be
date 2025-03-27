69c2f7d37ce377738a716a7dc5ff1a1f5f7b858e
Tran Hoang Phuc <phucth115.dev@gmail.com>
2025-03-28 00:48:10 +0700

    register service

---
+++ registry-service/src/test/java/fptu/sep490/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fptu.sep490.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
