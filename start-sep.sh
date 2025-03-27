c82caf24ead3886efc0fb6fd855bb1ef84eaa8a0
Tran Hoang Phuc <phucth115.dev@gmail.com>
2025-03-27 22:43:05 +0700

    Initial commit

---
+++ start-sep.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+if ! docker info > /dev/null 2>&1; then
+  echo "Start Docker Desktop to continue..."
+  exit 1
+fi
+
+echo "Initializing SEP services..."
+docker compose -f docker-compose.yml up -d --build
+
+echo "Check the status of SEP services..."
+docker ps
