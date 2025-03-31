c10e7121a2966e27b21a3cdc55fce3c74c637132
Tran Hoang Phuc <phucth115.dev@gmail.com>
2025-03-31 23:08:43 +0700

    add CustomAuditingEntityListener

---
+++ sample/src/main/java/fptu/sep490/sample/constants/PageableConstant.java
@@ -0,0 +1,6 @@
+package fptu.sep490.sample.constants;
+
+public class PageableConstant {
+    public static final String DEFAULT_PAGE_SIZE = "10";
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+}
