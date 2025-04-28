5f9650cabdaf110fd961fd19bfbd46da0c647136
tranHoang-Phuc <thphuc110523@gmail.com>
2025-04-28 07:53:06 +0700

    feature: add authentication token interceptor

---
+++ identity-service/src/main/java/com/fptu/sep490/identityservice/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.fptu.sep490.identityservice.config;
+
+import com.fptu.sep490.identityservice.component.AuthTokenInterceptor;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@FieldDefaults(level = AccessLevel.PRIVATE,  makeFinal = true)
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class WebConfig implements WebMvcConfigurer {
+    AuthTokenInterceptor authTokenInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authTokenInterceptor).addPathPatterns("/**");
+    }
+}
