2cfd56307d80100fa570aa549d465d3f39137090
tranHoang-Phuc <thphuc110523@gmail.com>
2025-05-23 17:08:23 +0700

    feature: add cors for localhost

--- common-library/src/main/java/com/fptu/sep490/commonlibrary/config/CorsConfig.java
+++ common-library/src/main/java/com/fptu/sep490/commonlibrary/config/CorsConfig.java
@@ -15,9 +15,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
+                        .allowedOrigins("http://localhost:3000")    // chỉ cho localhost:3000
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
