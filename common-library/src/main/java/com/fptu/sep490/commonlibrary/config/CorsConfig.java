74e3155920476a5332c29e5839949cc1f8c59280
tranHoang-Phuc <thphuc110523@gmail.com>
2025-05-23 17:18:55 +0700

    fix: allow all client

--- common-library/src/main/java/com/fptu/sep490/commonlibrary/config/CorsConfig.java
+++ common-library/src/main/java/com/fptu/sep490/commonlibrary/config/CorsConfig.java
@@ -17,8 +17,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
                         .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
