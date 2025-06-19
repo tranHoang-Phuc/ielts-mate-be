b9c5f75c579023844b14105c5000588276fcb98f
tranHoang-Phuc <thphuc110523@gmail.com>
2025-06-19 08:00:39 +0700

    fix: versioning dragItems

--- reading-service/src/main/java/com/fptu/sep490/readingservice/model/DragItem.java
+++ reading-service/src/main/java/com/fptu/sep490/readingservice/model/DragItem.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -33,4 +35,21 @@ public class DragItem {
             unique = true         // đảm bảo mỗi question_id chỉ dùng cho đúng 1 DragItem
     )
     private Question question;
+
+    @Column(name = "is_current")
+    private Boolean isCurrent = true;
+
+    @Column(name = "display_version")
+    private Integer version = 1;
+    @Column(name = "is_original")
+    private Boolean isOriginal = true;
+    @Column(name = "is_deleted")
+    private Boolean isDeleted = false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id", foreignKey = @ForeignKey(name = "fk_dragItem_parent"))
+    private DragItem parent;
+
+    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<DragItem> children = new ArrayList<>();
 }
--- reading-service/src/main/java/com/fptu/sep490/readingservice/repository/DragItemRepository.java
+++ reading-service/src/main/java/com/fptu/sep490/readingservice/repository/DragItemRepository.java
@@ -3,6 +3,7 @@ package com.fptu.sep490.readingservice.repository;
 import com.fptu.sep490.readingservice.model.DragItem;
 import com.fptu.sep490.readingservice.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.swing.text.html.Option;
@@ -16,8 +17,17 @@ import java.util.UUID;
 @Repository
 public interface DragItemRepository extends JpaRepository<DragItem, UUID> {
     Optional<DragItem> findDragItemByDragItemId(UUID dragItemId);
-    Optional<DragItem> findByDragItemIdAndQuestionGroup_GroupId(UUID dragItemId, UUID groupId);
+
+    Optional<DragItem> findByDragItemIdAndQuestionGroup_GroupIdAndIsDeleted(UUID dragItemId, UUID questionGroupGroupId, Boolean isDeleted);
     List<DragItem> findAllByQuestionGroup_GroupId(UUID groupId);
 
     Optional<DragItem> findByQuestion(Question question);
+
+    @Query("""
+        SELECT di FROM DragItem di 
+        WHERE (di.dragItemId = :originalDragItemId AND di.isCurrent = true)
+           OR (di.parent.dragItemId = :originalDragItemId AND di.isCurrent = true)
+    """)
+    List<DragItem> findPreviousDragItems(UUID originalDragItemId);
+
 }
