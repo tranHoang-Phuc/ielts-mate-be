9072fce46f9c3609abfea33945f66cb11c367c0d
tranHoang-Phuc <thphuc110523@gmail.com>
2025-07-21 07:56:34 +0700

    fix: duration

--- reading-service/src/main/java/com/fptu/sep490/readingservice/service/impl/AttemptServiceImpl.java
+++ reading-service/src/main/java/com/fptu/sep490/readingservice/service/impl/AttemptServiceImpl.java
@@ -265,7 +265,7 @@ public class AttemptServiceImpl implements AttemptService {
             );
         }
 
-        attempt.setDuration(attempt.getDuration());
+        attempt.setDuration(answers.duration());
 
         for (SavedAnswersRequest ans : answers.answers()) {
             Question question = questionRepository.findById(ans.questionId())
