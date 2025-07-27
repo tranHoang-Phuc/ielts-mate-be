2ae8b7df062d5f11231235431c2060b1fb8ed5a7
tranHoang-Phuc <thphuc110523@gmail.com>
2025-07-27 18:22:33 +0700

    fix: update listening

--- listening-service/src/main/java/com/fptu/sep490/listeningservice/service/impl/ListeningTaskServiceImpl.java
+++ listening-service/src/main/java/com/fptu/sep490/listeningservice/service/impl/ListeningTaskServiceImpl.java
@@ -206,7 +206,9 @@ public class ListeningTaskServiceImpl implements ListeningTaskService {
         newVersion.setVersion(version + 1);
         newVersion.setParent(task);
         newVersion.setIsOriginal(false);
-
+        listeningTaskRepository.saveAll(allVersion);
+        newVersion = listeningTaskRepository.save(newVersion);
+        listeningTaskRepository.save(task);
         if(!Objects.isNull(audioFile)) {
             fileService.uploadAsync("listening-tasks", audioFile, newVersion.getTaskId(), UUID.fromString(userId));
         } else {
