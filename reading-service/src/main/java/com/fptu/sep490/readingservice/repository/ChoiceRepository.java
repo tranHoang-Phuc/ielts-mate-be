50485819b72228c0ed3f5f7f7d60471839417328
tranHoang-Phuc <thphuc110523@gmail.com>
2025-08-26 22:22:17 +0700

    fix: get choices of question

--- reading-service/src/main/java/com/fptu/sep490/readingservice/repository/ChoiceRepository.java
+++ reading-service/src/main/java/com/fptu/sep490/readingservice/repository/ChoiceRepository.java
@@ -67,7 +67,7 @@ public interface ChoiceRepository extends JpaRepository<Choice, UUID> {
     @Query("""
         select c from Choice c
             where 
-                (c.choiceId IN (select ch.choiceId from Choice ch where ch.question.questionId = :questionId) and c.isOriginal = true and c.isCurrent = true and c.isDeleted =false)
+                (c.choiceId IN (select ch.choiceId from Choice ch where ch.question.questionId = :questionId)  and c.isCurrent = true and c.isDeleted =false)
                    or 
                 (c.choiceId IN (select ch.choiceId from Choice ch where ch.question.parent.questionId = :questionId)  and c.isCurrent = true and c.isDeleted =false)
                 
