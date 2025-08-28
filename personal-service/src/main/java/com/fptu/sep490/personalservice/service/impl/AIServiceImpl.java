481f060cd100cd03fbaa21b4d704936afdaafe9d
Tung <tungvxhe172415@fpt.edu.vn>
2025-08-28 13:29:48 +0700

    hot fix context

--- personal-service/src/main/java/com/fptu/sep490/personalservice/service/impl/AIServiceImpl.java
+++ personal-service/src/main/java/com/fptu/sep490/personalservice/service/impl/AIServiceImpl.java
@@ -306,7 +306,7 @@ public class AIServiceImpl implements AIService {
         StringBuilder prompt = new StringBuilder();
         prompt.append("You are a linguistic categorizer.\n");
         prompt.append("You are an advanced English-English dictionary.\n");
-        prompt.append("Based on the word and its meaning, return ONLY one short context/domain.\n");
+        prompt.append("Based on the word and its meaning, return short context/domain.\n");
         prompt.append("Rules:\n");
         prompt.append("- Do not explain.\n");
         prompt.append("- Do not output multiple options.\n\n");
